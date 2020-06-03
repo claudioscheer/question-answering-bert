f86a52cab5275e5e0d05292c1bd2865dca3626d3
Claudio Scheer <claudioscheer@protonmail.com>
2020-06-02 21:16:59 -0300

    Sequence-to-sequence model

--- src/seq2seq/main.py
+++ src/seq2seq/main.py
@@ -4,51 +4,18 @@ import pandas as pd
 from simpletransformers.seq2seq import Seq2SeqModel
 
 
-logging.basicConfig(level=logging.INFO)
-transformers_logger = logging.getLogger("transformers")
-transformers_logger.setLevel(logging.WARNING)
-
-dataset = []
-file_path = os.path.basename(os.path.dirname(__file__))
-with open(
-    os.path.join(file_path, "../../dataset/twitter-dev/twitter_en.txt"), "r"
-) as file:
-    dataset = file.readlines()
-
-train_data = []
-i = 0
-while i < len(dataset):
-    train_data.append([dataset[i], dataset[i + 1]])
-    i += 2
-
-train_df = pd.DataFrame(train_data, columns=["input_text", "target_text"])
-
-eval_data = [
-    [
-        "thinks you're mr. moon's son!",
-        "i'm okay with that. it had nothing to do with a bright orange astros jersey i was wearing 😉",
-    ],
-]
-eval_df = pd.DataFrame(eval_data, columns=["input_text", "target_text"])
-
-model_args = {
-    "fp16": False,
-    "reprocess_input_data": True,
-    "overwrite_output_dir": True,
-    "max_seq_length": 256,
-    "train_batch_size": 1,
-    "num_train_epochs": 50,
-    "save_eval_checkpoints": False,
-    "save_model_every_epoch": False,
-    "evaluate_generated_text": True,
-    "evaluate_during_training": False,
-    "evaluate_during_training_verbose": False,
-    "use_multiprocessing": False,
-    "save_best_model": False,
-    "max_length": 512,
-}
-
-model = Seq2SeqModel("bert", "bert-base-cased", "bert-base-cased", args=model_args)
+def get_dataset(file_path):
+    train = []
+    file_path = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(file_path, file_path), "r") as file:
+        train = file.readlines()
+    data = []
+    i = 0
+    while i < len(train):
+        data.append([train[i], train[i + 1]])
+        i += 2
+    df = pd.DataFrame(data, columns=["input_text", "target_text"])
+    return df
 
 
 def count_matches(labels, preds):
@@ -57,10 +24,41 @@ def count_matches(labels, preds):
     return sum([1 if label == pred else 0 for label, pred in zip(labels, preds)])
 
 
+logging.basicConfig(level=logging.INFO)
+transformers_logger = logging.getLogger("transformers")
+transformers_logger.setLevel(logging.WARNING)
+
+train_df = get_dataset("../../dataset/twitter-dev/twitter_en.train.txt")
+eval_df = get_dataset("../../dataset/twitter-dev/twitter_en.eval.txt")
+
+model_args = {
+    "fp16": False,
+    "overwrite_output_dir": True,
+    "max_seq_length": 256,
+    "train_batch_size": 16,
+    "eval_batch_size": 16,
+    "num_train_epochs": 100,
+    "save_eval_checkpoints": True,
+    "save_model_every_epoch": True,
+    "evaluate_generated_text": True,
+    "evaluate_during_training": True,
+    "evaluate_during_training_verbose": True,
+    "use_multiprocessing": False,
+    "save_best_model": True,
+    "max_length": 128,
+    "num_beams": 3,
+    "early_stopping": False,
+    "learning_rate": 1e-4,
+}
+
+model = Seq2SeqModel("bert", "bert-base-cased", "bert-base-cased", args=model_args)
+
 model.train_model(train_df, eval_data=eval_df, matches=count_matches)
 
 print("Evaluating model:")
 print(model.eval_model(eval_df, matches=count_matches))
 
-print("Generating prediction:")
-print(model.predict(["bre takin shots"]))
+# print("Generating prediction:")
+# output = model.predict(["bre takin shots"])[0]
+# print(output)
+# print(len(output))
