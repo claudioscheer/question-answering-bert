394ac375f4a5e93fc693d83d36f0df1441610a5f
Claudio Scheer <claudioscheer@protonmail.com>
2020-06-03 13:32:17 -0300

    Copy seq2seq model from simpletransformers

--- src/seq2seq/main.py
+++ src/seq2seq/main.py
@@ -1,7 +1,7 @@
 import os
 import logging
 import pandas as pd
-from simpletransformers.seq2seq import Seq2SeqModel
+from lib import Seq2SeqModel
 
 
 def get_dataset(dataset_path):
@@ -18,47 +18,27 @@ def get_dataset(dataset_path):
     return df
 
 
-def count_matches(labels, preds):
-    print(labels)
-    print(preds)
-    return sum([1 if label == pred else 0 for label, pred in zip(labels, preds)])
-
-
 logging.basicConfig(level=logging.INFO)
 transformers_logger = logging.getLogger("transformers")
 transformers_logger.setLevel(logging.WARNING)
 
-train_df = get_dataset("../../dataset/twitter-dev/twitter_en.train.txt")
-eval_df = get_dataset("../../dataset/twitter-dev/twitter_en.eval.txt")
+train_df = get_dataset("../../dataset/twitter-dev-small/twitter_en.train.txt")
 
 model_args = {
     "fp16": False,
     "overwrite_output_dir": True,
-    "max_seq_length": 256,
+    "max_seq_length": 128,
     "train_batch_size": 8,
-    "eval_batch_size": 8,
-    "num_train_epochs": 100,
-    "save_eval_checkpoints": True,
-    "save_model_every_epoch": True,
-    "evaluate_generated_text": True,
-    "evaluate_during_training": True,
-    "evaluate_during_training_verbose": True,
-    "use_multiprocessing": False,
-    "save_best_model": True,
+    "eval_batch_size": 1,
+    "num_train_epochs": 64,
     "max_length": 128,
     "num_beams": 3,
     "early_stopping": False,
     "learning_rate": 1e-4,
+    "save_eval_checkpoints": False,
+    "save_model_every_epoch": False,
+    "save_best_model": False,
 }
 
-model = Seq2SeqModel("bert", "bert-base-cased", "bert-base-cased", args=model_args)
-
-model.train_model(train_df, eval_data=eval_df, matches=count_matches)
-
-print("Evaluating model:")
-print(model.eval_model(eval_df, matches=count_matches))
-
-# print("Generating prediction:")
-# output = model.predict(["bre takin shots"])[0]
-# print(output)
-# print(len(output))
+model = Seq2SeqModel(encoder_type="bert", encoder_name="bert-base-cased", decoder_name="bert-base-cased", args=model_args)
+model.train_model(train_df)
