da0f59df4fb749d7eb2409e0b65c4703311e0076
Claudio Scheer <claudioscheer@protonmail.com>
2020-06-17 02:33:39 -0300

    Load model based on saved args

--- src/seq2seq/load_model.py
+++ src/seq2seq/load_model.py
@@ -11,21 +11,5 @@ logging.basicConfig(level=logging.INFO)
 transformers_logger = logging.getLogger("transformers")
 transformers_logger.setLevel(logging.WARNING)
 
-model_args = {
-    "fp16": False,
-    "overwrite_output_dir": True,
-    "max_seq_length": 256,
-    "train_batch_size": 8,
-    "num_train_epochs": 32,
-    "max_length": 256,
-    "learning_rate": 1e-4,
-    "save_eval_checkpoints": False,
-    "save_model_every_epoch": False,
-    "save_best_model": False,
-    "save_steps": 100000,
-    "num_beams": 5,
-    "gradient_accumulation_steps": 1,
-}
-
-model = Seq2SeqModel(encoder_decoder_type="bert", encoder_decoder_name="outputs", args=model_args)
+model = Seq2SeqModel(encoder_decoder_type="bert", encoder_decoder_name="outputs")
 print(model.predict([sys.argv[1]]))
